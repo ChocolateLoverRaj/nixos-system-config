f530dde5da648e7131d79a9e41da6ee993c30a0e
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-03-30 20:43:48 -0700

    Add meself to virt

--- virt-manager.nix
+++ virt-manager.nix
@@ -3,4 +3,6 @@
 {
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
+  # This is just to make it more connenient and not have to enter a password
+  users.users."rajas".extraGroups = [ "libvirtd" ];
 }
