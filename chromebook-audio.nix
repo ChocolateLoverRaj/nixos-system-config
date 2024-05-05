0a6322ed34d64e26447686affedbdba86c108925
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-05-05 10:34:09 -0700

    Common SOF chromebook audio
    Bug: no headphone mic

--- chromebook-audio.nix
+++ chromebook-audio.nix
@@ -2,86 +2,55 @@
 
 
 let
-  adl-ucm-conf = with pkgs; alsa-ucm-conf.overrideAttrs {
+  chromebook-ucm-conf = with pkgs; alsa-ucm-conf.overrideAttrs {
     wttsrc = fetchFromGitHub {
       owner = "WeirdTreeThing";
       repo = "chromebook-ucm-conf";
-      rev = "1328e46bfca6db2c609df9c68d37bb418e6fe279";
-      hash = "sha256-DDQgIgVyA+Kj5Di8aMltkk3dM0VIeT9/H0/+iL6IPMU=";
+      rev = "b6ce2a7";
+      hash = "sha256-QRUKHd3RQmg1tnZU8KCW0AmDtfw/daOJ/H3XU5qWTCc=";
     };
-    unpackPhase = ''
-      runHook preUnpack
-
-      tar xf "$src"
-
-      runHook postUnpack
-    '';
-    installPhase = ''
-      runHook preInstall
-
-      mkdir -p $out/share/alsa
-      cp -r alsa-ucm*/{ucm,ucm2} $out/share/alsa
-      cp -r $wttsrc/common $out/share/alsa/ucm2
-      cp -r $wttsrc/adl/* $out/share/alsa/ucm2/conf.d
-
-      runHook postInstall
+    postInstall = ''
+      cp -R $wttsrc/common/* $out/share/alsa/ucm2/common
+      cp -R $wttsrc/codecs/* $out/share/alsa/ucm2/codecs
+      cp -R $wttsrc/platforms/* $out/share/alsa/ucm2/platforms
+      cp -R $wttsrc/sof-rt5682 $out/share/alsa/ucm2/conf.d
+      cp -R $wttsrc/sof-cs42l42 $out/share/alsa/ucm2/conf.d
     '';
   };
 in
 {
-  boot = {
-    extraModprobeConfig = ''
-      options snd-intel-dspcfg dsp_driver=3
-    '';
-  };
-
   environment = {
     systemPackages = with pkgs; [
       maliit-keyboard
       sof-firmware
     ];
-    sessionVariables.ALSA_CONFIG_UCM2 = "${adl-ucm-conf}/share/alsa/ucm2";
-    # For nixos 23.11
-    # etc = {
-    #   "wireplumber/main.lua.d/51-increase-headroom.lua".text = ''
-    #     rule = {
-    #       matches = {
-    #         {
-    #           { "node.name", "matches", "alsa_output.*" },
-    #         },
-    #       },
-    #       apply_properties = {
-    #         ["api.alsa.headroom"] = 4096,
-    #       },
-    #     }
-
-    #     table.insert(alsa_monitor.rules,rule)
-    #   '';
-    # };
+    sessionVariables.ALSA_CONFIG_UCM2 = "${chromebook-ucm-conf}/share/alsa/ucm2";
   };
 
   system.replaceRuntimeDependencies = with pkgs; [
     ({
       original = alsa-ucm-conf;
-      replacement = adl-ucm-conf;
+      replacement = chromebook-ucm-conf;
     })
   ];
 
   # For nixos unstable
   services.pipewire.wireplumber.configPackages = [
-    (pkgs.writeTextDir "share/wireplumber/main.lua.d/51-increase-headroom.lua" ''
-      rule = {
-        matches = {
-          {
-            { "node.name", "matches", "alsa_output.*" },
-          },
-        },
-        apply_properties = {
-          ["api.alsa.headroom"] = 4096,
-        },
-      }
-
-      table.insert(alsa_monitor.rules,rule)
+    (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/51-increase-headroom.lua" ''
+      monitor.alsa.rules = [
+        {
+          matches = [
+            {
+              node.name = "~alsa_output.*"
+            }
+          ]
+          actions = {
+            update-props = {
+              api.alsa.headroom = 4096
+            }
+          }
+        }
+      ]
     '')
   ];
 }
