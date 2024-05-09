09ccdb5a9e2ddbd87df9a6c610345c904ac467e6
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-05-08 21:09:38 -0700

    Enable USB redirection in virt manager

--- virt-manager.nix
+++ virt-manager.nix
@@ -1,17 +1,20 @@
 { pkgs, ... }:
 
 {
-  virtualisation.libvirtd = {
-    enable = true;
-    # Enable TPM, needed for Windows 11
-    qemu = {
-      package = pkgs.qemu_kvm;
-      ovmf = {
-        enable = true;
-        packages = [ pkgs.OVMFFull.fd ];
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      # Enable TPM, needed for Windows 11
+      qemu = {
+        package = pkgs.qemu_kvm;
+        ovmf = {
+          enable = true;
+          packages = [ pkgs.OVMFFull.fd ];
+        };
+        swtpm.enable = true;
       };
-      swtpm.enable = true;
     };
+    spiceUSBRedirection.enable = true;
   };
   programs.virt-manager.enable = true;
   # This is just to make it more connenient and not have to enter a password
