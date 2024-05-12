686a83e4a3471e131bd169b21678fa2a4ffed955
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-05-11 22:54:56 -0700

    Add ectool as build-input for auto-stop-charging

--- auto-stop-charging.nix
+++ auto-stop-charging.nix
@@ -1,20 +1,20 @@
 { pkgs, ... }:
 
 let
-  auto-stop-charging = with pkgs; rustPlatform.buildRustPackage
-    rec {
-      pname = "auto-stop-charging";
-      version = "1.0.0";
-      cargoHash = "sha256-VDMjhVX1S/UqiMyfGHT8R06V7h4SJchqBRi2JYKfaCk=";
-      src = fetchFromGitHub {
-        owner = "ChocolateLoverRaj";
-        repo = "cros-ec-battery";
-        rev = "3c6f320cee42150808387aca37679596a1c7a7e1";
-        hash = "sha256-Gv7uHf4dknYw+7tUp5JdHKUlmRDixWC49dp3y/dTwIc=";
-      };
-      sourceRoot = "${src.name}/auto-stop-charging";
-    };
   ectool = (pkgs.callPackage ./cros-ectool.nix { });
+  auto-stop-charging = with pkgs; rustPlatform.buildRustPackage rec {
+    pname = "auto-stop-charging";
+    version = "1.0.0";
+    cargoHash = "sha256-VDMjhVX1S/UqiMyfGHT8R06V7h4SJchqBRi2JYKfaCk=";
+    src = fetchFromGitHub {
+      owner = "ChocolateLoverRaj";
+      repo = "cros-ec-battery";
+      rev = "3c6f320cee42150808387aca37679596a1c7a7e1";
+      hash = "sha256-Gv7uHf4dknYw+7tUp5JdHKUlmRDixWC49dp3y/dTwIc=";
+    };
+    sourceRoot = "${src.name}/auto-stop-charging";
+    buildInputs = [ ectool ];
+  };
 in
 {
   environment.systemPackages = with pkgs; [
