70788c158940563cfdce131c0f6dd9a0f724c682
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-06-11 13:49:43 -0700

    Organize into multiple hosts

--- ipu6.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-
-{
-  # Doesn't work yet but hopefully one day after an update it'll magically start working
-  hardware.ipu6 = {
-    enable = true;
-    platform = "ipu6ep";
-  };
-}
