60c243393c109b34712fcc510d69bf3465e1ca75
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-12-12 22:02:23 -0800

    Change debugging to only specific device

--- debugging.nix
+++ debugging.nix
@@ -15,7 +15,7 @@
     (writeTextFile {
       name = "All USB Devices udev Rules";
       text = ''
-        SUBSYSTEM=="usb", MODE="0660", GROUP="diy_usb_device", ATTRS{idVendor}=="0xc0de", ATTRS{idProduct}=="0xcafe"
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="0xc0de", ATTRS{idProduct}=="0xcafe, MODE="0660", GROUP="diy_usb_device""
       '';
       destination = "/etc/udev/rules.d/70-all-usb.rules";
     })
