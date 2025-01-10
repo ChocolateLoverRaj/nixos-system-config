b76c86d5699e91c47c8bbe4092bdb321f515e028
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-01-10 09:18:45 -0800

    Update cros fp

--- cros-fp.nix
+++ cros-fp.nix
@@ -1,51 +1,23 @@
 { pkgs, lib, ... }:
 
 {
-  # nixpkgs.overlays = [
-  #   (final: prev:
-  #     {
-  #       libfprint = prev.libfprint.overrideAttrs (old: {
-  #         version = "1.94.8-crfpmoc";
-  #         src = fetchFromGitLab {
-  #           domain = "gitlab.freedesktop.org";
-  #           owner = "fandango96a";
-  #           repo = "libfprint";
-  #           rev = "feature/crfpmoc";
-  #           hash = null;
-  #         };
-  #       });
-  #     })
-  # ];
-
-  # system.replaceDependencies.replacements = with pkgs; [
-  #   ({
-  #     original = libfprint;
-  #     replacement = libfprint.overrideAttrs (old: {
-  #       version = "1.94.8";
-  #       src = pkgs.fetchFromGitLab {
-  #         domain = "gitlab.freedesktop.org";
-  #         owner = "fandango96";
-  #         repo = "libfprint";
-  #         rev = "56dc7f7524dabc0da55f2a15f7706e73778aa5e7";
-  #         hash = "sha256-ySifkClM6qjDlm8iPMwWngHs5PrB1reddreziIUEs5k=";
-  #       };
-  #     });
-  #   })
-  # ];
-
   nixpkgs.overlays = [
     (self: super: {
       libfprint = super.libfprint.overrideAttrs (oldAttrs: rec {
-        src = pkgs.fetchFromGitHub {
+        src = pkgs.fetchFromGitLab {
+          domain = "gitlab.freedesktop.org";
           owner = "Xelef2000";
           repo = "libfprint";
-          rev = "1821b98e7e4db05302b41b40ae628842c93169b6";
-          hash = "sha256-gvJJY8uVkPn8ASkqh4qE+SSqStnYlLVrcUITilQPf6U=";
+          rev = "779e82b1e4298996edc1d0eab6b5b97b92494e2f";
+          hash = "sha256-KudADZ5zdtyBsfar8FeFRDSuR0ztLbhEWy9pCV4MkWE=";
         };
       });
     })
   ];
 
+  environment.systemPackages = with pkgs; [
+    fprintd
+  ];
 
   services.fprintd = {
     enable = true;
