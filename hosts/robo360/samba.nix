c712fb7d60272a5ef9a258eb3235b1bab6db3e9b
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-08-21 08:29:44 -0700

    Update Samba for more ZFS datasets

--- hosts/robo360/samba.nix
+++ hosts/robo360/samba.nix
@@ -13,13 +13,33 @@
         "map to guest" = "Bad Password";
         # Only allow SMB on home network because it's not recommended to expose SMB to the internet
         "hosts allow" = "192.168. 127.0.0.1 localhost 100.118.30.62";
-        # This stops Samba from creating annoying extended attributes for every file created / copied through Samba  
+        # This stops Samba from creating annoying extended attributes for every file created / copied through Samba
         "store dos attributes" = "no";
         # This makes it so that if the client writes and waits for physical disk write, Samba will actually wait until the physical disk is written to before saying it's done writing.
         "strict sync" = "yes";
       };
       "Parasave" = {
         "path" = "/mnt/para-z/para-save";
+        "browseable" = "yes";
+        "writeable" = "yes";
+        # Allow access without username and password
+        "guest ok" = "yes";
+        # This makes it so that files created through SMB are accessible by all users
+        "create mask" = "0777";
+        "directory mask" = "0777";
+      };
+      "Entertainment" = {
+        "path" = "/mnt/para-z/entertainment";
+        "browseable" = "yes";
+        "writeable" = "yes";
+        # Allow access without username and password
+        "guest ok" = "yes";
+        # This makes it so that files created through SMB are accessible by all users
+        "create mask" = "0777";
+        "directory mask" = "0777";
+      };
+      "Old Hard Drive from Aazoba" = {
+        "path" = "/mnt/para-z/old-hard-drive-from-aazoba";
         "browseable" = "yes";
         "writeable" = "yes";
         # Allow access without username and password
