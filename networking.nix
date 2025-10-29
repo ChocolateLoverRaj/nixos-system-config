bfd55452ac042c915e235402218bc1f6a11b2403
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-10-28 18:18:52 -0700

    Open ports for Minecraft

--- networking.nix
+++ networking.nix
@@ -5,9 +5,20 @@
   networking.networkmanager.enable = true;
   # networking.networkmanager.wifi.backend = "iwd";
   # networking.wireless.iwd.enable = true;
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [ 36569 ];
-    allowedUDPPorts = [ 36569 ];
-  };
+  networking.firewall =
+    let
+      allowedCommonPorts = [
+        # Minecraft open to LAN
+        36569
+        # Minecraft Server
+        25565
+        # Minecraft mod - simple voice chat
+        24454
+      ];
+    in
+    {
+      enable = true;
+      allowedTCPPorts = allowedCommonPorts;
+      allowedUDPPorts = allowedCommonPorts;
+    };
 }
