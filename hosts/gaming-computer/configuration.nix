8c52de285493d359a65e871d3d06a03789dba4b6
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-12-20 23:21:35 -0800

    Update gaming comptuer

--- hosts/gaming-computer/configuration.nix
+++ hosts/gaming-computer/configuration.nix
@@ -25,6 +25,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
-
-  boot.kernelPackages = pkgs.linuxPackages_latest;
 }
