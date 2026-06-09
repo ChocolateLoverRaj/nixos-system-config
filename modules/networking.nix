1cefa5edf7aeb8227a6322381a2e45c02156225f
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2026-06-09 14:49:27 -0700

    Enable netbird

--- modules/networking.nix
+++ modules/networking.nix
@@ -40,6 +40,22 @@
     options cfg80211 ieee80211_regdom="US"
   '';
 
+  services.netbird = {
+    clients.wt0 = {
+      # Port used to listen to wireguard connections
+      port = 51821;
+
+      # Set this to true if you want the GUI client
+      ui.enable = false;
+
+      # This opens ports required for direct connection without a relay
+      openFirewall = true;
+
+      # This opens necessary firewall ports in the Netbird client's network interface
+      openInternalFirewall = true;
+    };
+  };
+
   # environment.etc = {
   #   "NetworkManager/dnsmasq-shared.d/tftp.conf".text = ''
   #     log-dhcp
